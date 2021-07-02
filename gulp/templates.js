5ed5fde1907491fefa716acb33a7b667050f0f45
Johan Palmfjord <johan@pogi.se>
2021-07-02 08:39:33 +0200

    Add part 2 step 2

--- gulp/templates.js
+++ gulp/templates.js
@@ -1,4 +1,5 @@
 import toml from 'toml'
+import marked from 'marked'
 import pug from 'pug'
 import { Transform } from 'stream'
 import path from 'path'
@@ -10,9 +11,28 @@ export const templates = ({ baseDir, templatesDir }) => {
   stream._transform = async function (chunk, _unused, callback) {
     const config = toml.parse(chunk.contents.toString()) // `chunk` represents the .toml file we're transforming into a template
     const templatePath = path.resolve(templatesDir, config.template) // inside the TOML file we specify which template file to use
+
     let result
     try {
-      result = pug.compileFile(templatePath)(config) // By passing the config (TOML file) to the template all configuration keys will be available as variables
+      const markdownedConfig = Object.entries(config) // Loop over every configuration key, as we want to check whether it should be parsed as markdown.
+        .map(([key, value]) => {
+          // If we prefix a string config value with `md:` that means we want to parse it as markdown
+          if (typeof value === 'string' && value.startsWith('md:')) {
+            return [key, marked(value.replace('md:', ''), {})] // remove the `md:` prefix and parse the rest as markdown. `.replace` will only replace the first occurrence so no worries if we happen to include `md:` in the actual content
+          } else if (
+            typeof value === 'string' &&
+            // If we prefix the string config value with `inlinemd:` that means we want to parse it as inline markdown
+            value.startsWith('inlinemd:')
+          ) {
+            return [key, marked.parseInline(value.replace('inlinemd:', ''), {})] // same thing as the `md:` prefix, but od it inline
+          } else {
+            // This should not be parsed as markdown
+            return [key, value]
+          }
+        })
+        .reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {}) // Put together the configuration as it was before, but with the parsed markdown
+
+      result = pug.compileFile(templatePath)(markdownedConfig)
     } catch (e) {
       callback(e) // Oops, tell Gulp sometihng went wrong by passing the error to the callback
       return
